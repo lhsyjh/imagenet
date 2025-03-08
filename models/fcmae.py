e45ffcaef6eef644d7eb01039a74938e506b729b
lhsyjh <lilylianghao@gmail.com>
2025-03-08 14:12:08 -0500

    Update fcmae.py

--- models/fcmae.py
+++ models/fcmae.py
@@ -97,9 +97,9 @@ class FCMAE(nn.Module):
         assert imgs.shape[2] == imgs.shape[3] and imgs.shape[2] % p == 0
 
         h = w = imgs.shape[2] // p
-        x = imgs.reshape(shape=(imgs.shape[0], 200, h, p, w, p))  #改200
+        x = imgs.reshape(shape=(imgs.shape[0], 3, h, p, w, p))
         x = torch.einsum('nchpwq->nhwpqc', x)
-        x = x.reshape(shape=(imgs.shape[0], h * w, p**2 * 200))    #改200
+        x = x.reshape(shape=(imgs.shape[0], h * w, p**2 * 3))
         return x
 
     def unpatchify(self, x):

7372038da7be0446bf236e47d8021f51ecd9729c
lhsyjh <lilylianghao@gmail.com>
2025-03-24 22:36:18 -0400

    Update engine_finetune.py

--- engine_finetune.py
+++ engine_finetune.py
@@ -156,7 +156,8 @@ def evaluate(data_loader, model, device, use_amp=False):
         metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
         metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
     # gather the stats from all processes
-    metric_logger.synchronize_between_processes()
+    if torch.cuda.is_available():
+        metric_logger.synchronize_between_processes()
     print('* Acc@1 {top1.global_avg:.3f} Acc@5 {top5.global_avg:.3f} loss {losses.global_avg:.3f}'
           .format(top1=metric_logger.acc1, top5=metric_logger.acc5, losses=metric_logger.loss))
 
